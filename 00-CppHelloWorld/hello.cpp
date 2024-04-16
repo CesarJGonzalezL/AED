73e53d68cc653904f77c70e31a9aca963bb6a1f7
CesarJGonzalezL <cgonzalezlaffont@frba.utn.edu.ar>
2024-04-15 21:20:17 -0300

    Added: TP00-CppHelloWorld

---
+++ 00-CppHelloWorld/hello.cpp
@@ -0,0 +1,16 @@
+/* Hello World
+* César J. González L.
+* 20240415
+*/
+
+#include <iostream>
+int main() {
+    std::cout << "Hello, World!\n";
+}
+
+//Compilar ( " -o " : nombre del archivo .out)
+//g++ hello.cpp -o hello.out
+//Ejecutar en directorio donde esta el .out
+//./hello.out
+//Redireccionar la salida a un txt
+//./hello.out > hello.txt
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# AED
+# Algoritmos-y-Estructuras-de-Datos
+
+## Información del Curso
+- **Curso:** *K1253 Miércoles noche*
+- **Año de cursada:** *2024*
+- **Cuatrimestre:** *1ro.*
+- **Legajo:** *220.517-8*
+- **Apellido:** *González L.*
+- **Nombre:** *César J.*
+
+### Repositorio público personal para la asignatura
